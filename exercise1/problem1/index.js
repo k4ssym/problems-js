660b98077b424f40dc4afa8de18128b07e0cf1ff
k4ssym <136691771+k4ssym@users.noreply.github.com>
2023-09-03 11:14:29 +0600

    Update index.js

--- exercise1/problem1/index.js
+++ exercise1/problem1/index.js
@@ -1,8 +1,4 @@
-function numOfDigits(int) {
-  if (num === 0) {
-    return 1; // Special case for 0
-  }
-  return Math.floor(Math.log10(Math.abs(num))) + 1;
+function numOfDigits(num) {
+  const numStr = num.toString();
+  return numStr.length;
 }
-}
-module.exports = numOfDigits;
